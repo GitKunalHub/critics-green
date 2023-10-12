7e41242734595d8d1479e81ed7d70680568d7f72
Kunal <kunalgolani.work@gmail.com>
2023-10-13 01:02:23 +0530

    Displaying Critic Score.

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Movie } from "../hooks/useMovie";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import CriticScore from "./CriticScore";
 const api_img = "https://image.tmdb.org/t/p/w500/";
 interface Props {
   movie: Movie;
@@ -11,7 +12,10 @@ const MovieCard = ({ movie }: Props) => {
     <Card borderRadius={10} overflow={"hidden"}>
       <Image src={ api_img + movie.poster_path} />
       <CardBody>
+        <HStack justifyContent={"space-between"}>
         <Heading fontSize={"2xl"}>{movie.title}</Heading>
+        <CriticScore score={movie.vote_average} />
+        </HStack>
       </CardBody>
     </Card>
   );
