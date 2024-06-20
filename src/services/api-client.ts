7c7498c6d38c6d88af0dee29b8a5b693e2dee7d6
Kunal <kunalgolani.work@gmail.com>
2024-06-20 18:53:08 +0530

    Updated API-Key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "a88310444f31ff04f8d88dd91490876f",
+    api_key: "5149716bfb53e14a9dba8b66c87fcfd3",
   },
 });
